7da0d5903fca517b77d2d9f86e14f75ed0e2f466
Andrew Frederick <andrewjfrederick@gmail.com>
2024-01-30 23:24:17 -0800

    init

---
+++ force-app/main/default/lwc/accountStockData/accountStockData.js
@@ -0,0 +1,33 @@
+import { LightningElement, api, wire } from 'lwc';
+
+import getStockData from '@salesforce/apex/Controller_AccountStockData.getStockData';
+
+export default class AccountStockData extends LightningElement {
+
+    @api recordId;
+
+    marketDate = new Date().toISOString().split('T')[0];
+
+    stockData = {
+        open : '',
+        high : '',
+        low : '',
+        close : '',
+    };
+
+    @wire( getStockData, { accId : '$recordId', mDate : '$marketDate' } )
+    wiredStockDate({data, error}){
+
+        if( data ){
+            this.stockData = data;
+        } else
+        if( error ){
+            console.error(error);
+        }
+    }
+
+    handleDateChange( event ){
+        console.log(event.currentTarget.value);
+        this.marketDate = event.currentTarget.value;
+    }
+}
